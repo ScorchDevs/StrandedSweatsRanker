0e446d44c13b08b21e91bb5e7acfe532802a3773
Lars <l.cornelissen5@student.han.nl>
2022-02-17 23:19:08 +0100

    Moved around some shenanigans

--- API/api_v1.py
+++ API/api_v1.py
@@ -9,9 +9,9 @@ api = Api(app)
 api.add_resource(Main.Main, '/v1/')
 
 api.add_resource(Player.Players, '/v1/players/')
-api.add_resource(Player.Player, '/v1/players/<uuid>/stats')
-api.add_resource(Player.PlayerWeight, '/v1/players/<uuid>/weights')
+api.add_resource(Player.Player, '/v1/players/<uuid>')
 
+api.add_resource(Player.PlayerWeight, '/v1/weights/<uuid>')
 api.add_resource(Weights.Weight, '/v1/weights/version/<version>')
 api.add_resource(Weights.Weights, '/v1/weights/')
 api.add_resource(Weights.WeightsTop, '/v1/weights/top/<name>')
