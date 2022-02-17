e94a627238beeb9cda2aec427f8b3529739f51a5
Lars <l.cornelissen5@student.han.nl>
2022-02-17 21:18:52 +0100

    API Version control

--- API/api_v1.py
+++ API/api_v1.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Api
-from endpoints import *
+from endpoints.v1 import *
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Main.Main, '/')
-api.add_resource(Player.Player, '/player/')
+api.add_resource(Main.Main, '/v1/')
+api.add_resource(Weights.Weights, '/v1/weights/')
+api.add_resource(Player.Player, '/v1/player/')
 
 
 
