9e1c4de8c0dcafbee81779a5756a189dc2500a08
Lars Cornelissen <l.cornelissen5@student.han.nl>
2022-02-19 10:52:16 +0100

    Added favicon GET endpoint

--- API/api_v1.py
+++ API/api_v1.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from endpoints.v1 import *
-
+from flask import send_from_directory
+import os
 
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 api.add_resource(Main.Main, '/v1/')
 api.add_resource(Auth.SignUpForAPI, '/v1/auth/signup')
