e0fe0ca325151faaadbb815c98a773017a4b0f70
Lars Cornelissen <l.cornelissen5@student.han.nl>
2022-02-19 11:30:13 +0100

    Refactored /auth/allow/<username> endpoint

--- API/endpoints/v1/Auth.py
+++ API/endpoints/v1/Auth.py
@@ -26,11 +26,15 @@ class SignUpForAPI(Resource):
 def checkPassword(given_password, hashed_password):
     try:
         return check_password_hash(hashed_password, given_password)
-    except:
+    except Exception as e:
         return False
+
+def isUserAdmin(username):
+    return db.selectQuery("SELECT is_admin FROM api_users WHERE username LIKE ?", params=(username,))[0]
+
 def retrievePwdHashForUser(username):
     try:
-        return db.selectQuery("SELECT password FROM api_users WHERE username LIKE ? AND is_admin = 1", params=(username,))[0]
+        return db.selectQuery("SELECT password FROM api_users WHERE username LIKE ?", params=(username,))[0]['password']
     except:
         return {"password": ""}
 
@@ -39,14 +43,24 @@ def retrievePwdHashForUser(username):
 class AllowUsername(Resource):
     def post(self, username):
         body = request.get_json()
-        if not body: return {"message": "Missing parameters!"}, 400
-        if "username" not in body: return {"message": "Missing username!"}, 400
-        if "password" not in body: return {"message": "Missing password!"}, 400
-        admin_pwd_hash = retrievePwdHashForUser(body['username'])["password"]
-
-        if not admin_pwd_hash: return {"message": "You are not an administrator!"}
-        admin_logged_in = checkPassword(body['password'], admin_pwd_hash)
-        if not admin_logged_in: return {"message": "Password incorrect!"}
-
-        db.executeQuery("UPDATE api_users SET allowed_to_use_api = 1 WHERE username = ?", params=(username,))
+        logged_in, error = checkUserCombinationCorrect(body)
+        if not logged_in: return {"message": "Something went wrong: " + error}
+        is_admin = isUserAdmin(body['username'])
+        if not is_admin: return {"message": "You are not an administrator!"}
+        try:
+            db.executeQuery("UPDATE api_users SET allowed_to_use_api = 1 WHERE username = ?", params=(username,))
+        except:
+            return {"message": "Something unexpected went wrong!"}
         return {"message": "User successfully added to allow list!"}
+
+
+
+
+def checkUserCombinationCorrect(login: dict): # Should look like: {"username": "name", "password": "HasHEd PAssWord"}
+    if not login: return False, "No body given!"
+    if "username" not in login: return False, "No username given"
+    if "password" not in login: return False, "No password given"
+    hashed_password = retrievePwdHashForUser(login["username"])
+    password_correct = checkPassword(login['password'], hashed_password)
+    if not password_correct: return False, "Password incorrect"
+    return True, ""
